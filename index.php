d769bf2f1ff8653f1b4364975af6bbffb8449775
Nathaniel Higgins <nat@nath.is>
2011-09-11 01:27:20 +0100

    Base

--- index.php
+++ index.php
@@ -7,19 +7,30 @@
 	<script type="text/javascript" src="jquery.scrollbars.js"></script>
 	<script type="text/javascript">
 	$(document).ready(function() {
-		$('#div-1').scrollbars();
+		$('div').scrollbars();
 	});
 	</script>
 	<style type="text/css">
-	#div-1 {
+	@import url(jquery.scrollbars.css);
+	#div-1, #div-2 {
 		width: 300px;
 		height: 180px;
 		overflow-y: scroll;
+		float: left;
+		margin-right: 30px;
 	}
 	</style>
 </head>
 <body>
 	<div id="div-1">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</div>
+	<div id="div-2">
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
