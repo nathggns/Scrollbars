0f41fde17f5115c710f459d0c75f78b6b7124268
Nathaniel Higgins <nat@nath.is>
2011-09-23 22:38:49 +0100

    First "working" version

--- jquery.scrollbars.js
+++ jquery.scrollbars.js
@@ -1,2 +1,145 @@
 (function($) {
+	var data = {};
+
+	var methods = {
+		init: function() {
+			// Test if we need to use our plugin
+			if (this.css('overflow') != 'auto' && this.css('overflow') != 'hidden') return;
+
+			// Make sure we don't have position static
+			if (this.css('position') === 'static') this.css('position', 'relative');
+
+			// Lock size
+			this.css({
+				height: this.height(),
+				width: this.width()
+			});
+
+			// Apply overflow hidden (remove scrollbars)
+			this.css({
+				overflow: 'hidden'
+			});
+
+			// Generate a unique id for every element generated
+			id = 'scroll-' + Math.floor(Math.random() * 10000);
+
+			// Call generate function
+			methods.generate.apply(this, Array(id));
+
+			// Add our event listeners
+			methods.addEvents.apply(this);
+		},
+		generate: function(id) {
+			this.addClass('scrollRoot').addClass(id);
+			// Create a root wrap to make space for scrollbars
+			rootWrap = $(document.createElement('div'));
+			rootWrap
+				.addClass(id)
+				.addClass('scrollElement')
+				.addClass('rootWrap')
+				.html(this.html())
+				.css({
+					height: this.height(),
+					width: this.width() - 20
+				});
+			
+			this.html('').append(rootWrap);
+
+			// Create a content wrap for various calculations and manipulations
+			contentWrap = $(document.createElement('div'));
+			contentWrap
+				.addClass(id)
+				.addClass('scrollElement')
+				.addClass('contentWrap')
+				.html(rootWrap.html());
+			
+			rootWrap.html('').append(contentWrap);
+
+			// Create a container for the dragger
+			dragCon = $(document.createElement('div'));
+			dragCon
+				.addClass(id)
+				.addClass('scrollElement')
+				.addClass('dragCon');
+			
+			this.append(dragCon);
+
+			// Work out a ratio
+			ratio = rootWrap.height() / contentWrap.height();
+			dragHeight = +(rootWrap.height() * ratio);
+
+			// Create our dragger
+			drag = $(document.createElement('div'));
+			drag
+				.addClass(id)
+				.addClass('scrollElement')
+				.addClass('drag')
+				.css({
+					height: dragHeight
+				})
+				.data('id', id)
+				.data('ratio', ratio);
+
+			dragCon.append(drag);
+		},
+		addEvents: function() {
+			drag = this.find('.drag');
+			contentWrap = this.find('.contentWrap');
+
+			drag.mousedown(function(event) {
+				$(this).data('move', event.pageY);
+				$('body').css({
+					cursor: 'normal'
+				});
+				event.preventDefault();
+			});
+
+			$('*').mouseup(function(event) {
+				$('.scrollElement.drag').data('move', false);
+			}).mousemove(function(event) {
+				$('.scrollElement.drag').each(function() {
+					if (!$(this).data('move')) return;
+					offset = event.pageY - $(this).data('move');
+					$(this).data('move', event.pageY);
+
+					methods.move.call($('.scrollRoot.' + $(this).data('id')), offset);
+				});
+			})
+		},
+		move: function(offset) {
+			drag = this.find('.drag');
+			contentWrap = this.find('.contentWrap');
+			rootWrap = this.find('.rootWrap');
+
+			current = drag.css('top');
+			current = current == 'auto' ? 0 : parseFloat(current);
+
+			distance = current + offset;
+
+			min = 0;
+			max = drag.parent().height() - drag.height();
+
+			if (distance < min) distance = min;
+			if (distance > max) distance = max;
+
+			drag.css({
+				'top': distance
+			});
+
+			trackDistance = rootWrap.height() - drag.height();
+			notVisible = contentWrap.height() - rootWrap.height();
+			distanceRatio = notVisible / trackDistance
+
+			distance = (distance * distanceRatio) * -1;
+
+			contentWrap.css({
+				top: distance
+			});
+		}
+	}
+	$.fn.scrollbars = function() {
+		return this.each(function() {
+			methods.init.apply($(this), arguments);
+		});
+	}
 })(jQuery);
